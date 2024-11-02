4234f0ba59824c5a8606fa7206b14e7e2bd360d9
pauromero25 <pauromero205@gmail.com>
2024-11-02 20:15:58 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,33 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(246, 255, 245);
+  fill(155);
+  stroke(3);
+  strokeWeight(1);
+  rect(135,40,130,50,5)
+  
+    fill(192, 207, 193);
+  stroke(3);
+  strokeWeight(1);
+  rect(100,90,200,280,40)
+  
+  fill(111, 166, 116)
+  stroke(0)
+  rect(100,160,200,100)
+ 
+    stroke(0);
+  strokeWeight(1.5);
+  fill(0)
+  textSize(20)
+  text("Herbolario la Hierbas", 107,200)
+  stroke(0)  
+  strokeWeight(0.5)
+  fill(30)
+  textSize(15)
+  text("Remedios naturales", 135,225)
+  textSize(10)
+  text("por Pau Romero",165,360)
+}
